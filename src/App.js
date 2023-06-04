8c2bbbc9f7902aa32a3aae70190a1f33c9ccf0b0
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-06-04 13:29:36 +0430

    some chnages

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Explore />} />
         <Route path="/offers" element={<Offers />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<SignIn />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="sign-up" element={<SignUp />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
       </Routes>
       <Navbar />
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
 import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
 import { ReactComponent as PersonOutlineIcon } from "../assets/svg/personOutlineIcon.svg";
-
 function Navbar() {
-  // in order to make use of this hooks we need to initilzei them
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -21,8 +19,8 @@ function Navbar() {
           <li className="navbarListItem" onClick={() => navigate("/")}>
             <ExploreIcon
               fill={pathMatchRoute("/") ? "#2c2c2c" : "#8f8f8f"}
-              width="36px"
-              height="36px"
+              width={"36px"}
+              height={"36px"}
             />
             <p
               className={
@@ -37,9 +35,8 @@ function Navbar() {
           <li className="navbarListItem" onClick={() => navigate("/offers")}>
             <OfferIcon
               fill={pathMatchRoute("/offers") ? "#2c2c2c" : "#8f8f8f"}
-              width="36px"
-              height="36px"
-              onClick={() => navigate("/offers")}
+              width={"36px"}
+              height={"36px"}
             />
             <p
               className={
@@ -53,10 +50,9 @@ function Navbar() {
           </li>
           <li className="navbarListItem" onClick={() => navigate("/profile")}>
             <PersonOutlineIcon
+              width={"36px"}
+              height={"36px"}
               fill={pathMatchRoute("/profile") ? "#2c2c2c" : "#8f8f8f"}
-              width="36px"
-              height="36px"
-              onClick={() => navigate("/profile")}
             />
             <p
               className={
