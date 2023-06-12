495cd40db0dea0d122708c7531802f2b3600b868
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-06-12 16:12:28 +0430

    adding forgot password section

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { useAuthStatus } from "../hooks/useAuthStatus";
 import { Navigate, Outlet } from "react-router-dom";
+import Spinner from "./Spinner";
+// outlet allows us to render child compienet
+// any route that you want to make it private you need to use outlet
 const PrivateRoute = () => {
-  const loggedIn = false;
+  const { loggedIn, checkingStatus } = useAuthStatus();
+
+  if (checkingStatus) {
+    return (
+      <h3>
+        <Spinner />
+      </h3>
+    );
+  }
   return loggedIn ? <Outlet /> : <Navigate to={"/sign-in"} />;
 };
+
+// Here you may fix memory lead warning
 
 export default PrivateRoute;
--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+// we need to have link to the sign-in page
 
 function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent");
+      navigate("/sign-in");
+    } catch (error) {
+      toast.error("Could not send reset Email");
+    }
+  };
+
   return (
-    <div>
-      <h1>ForgotPassword </h1>
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Forgot Password</p>
+      </header>
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            className="emailInput"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+          <Link to={"/sign-in"} className="forgotPasswordLink">
+            Sign In
+          </Link>
+          <div className="signInBar">
+            <div className="signInText">Send Reset Link</div>
+            <button className="signInButton">
+              <ArrowRightIcon fill="#fff" width={"34px"} height={"34px"} />
+            </button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 }
