f79db743b7b20206074e87b8a5c9f50bdee4ff30
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-07-01 22:09:26 +0430

    changing firebase

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Explore from "./pages/Explore";
-import ForgotPassword from "./pages/ForgotPassword";
-import Offers from "./pages/Offers";
-import Profile from "./pages/Profile";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
-import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Explore />} />
-        <Route path="/offers" element={<Offers />} />
-        <Route path="/profile" element={<SignIn />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-      </Routes>
-      <Navbar />
-    </>
-  );
+  return <div>App</div>;
 }
 
 export default App;
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,20 +1,15 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-//we need to import firestore as database
 import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA0l_mJgQpfidi7_aWAKGgGfjv18ESUirQ",
-  authDomain: "house-marketplace-app-3c9b4.firebaseapp.com",
-  projectId: "house-marketplace-app-3c9b4",
-  storageBucket: "house-marketplace-app-3c9b4.appspot.com",
-  messagingSenderId: "743396973426",
-  appId: "1:743396973426:web:8037b27f663ada0e261beb",
+  apiKey: "AIzaSyD9OArLpUawfAccbmHsobhRe-xDgvbQGtM",
+  authDomain: "house-market-app-e02fc.firebaseapp.com",
+  projectId: "house-market-app-e02fc",
+  storageBucket: "house-market-app-e02fc.appspot.com",
+  messagingSenderId: "750879269946",
+  appId: "1:750879269946:web:e6af9ab94be6dd3e73c103",
 };
 
-// we pass firebaseConfig to initlize app
-
 // Initialize Firebase
-//we are passing api key asnd project id and all the information within the project
-initializeApp(firebaseConfig);
-
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
